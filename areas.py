24026e4041923a699c70cc0c5ca05b89ab9f523e
Hector Sanchez-Moran <67207265+hector-s-m@users.noreply.github.com>
2023-05-07 02:46:03 +0200

    Areas.py

--- areas.py
+++ areas.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 cmd.set('dot_solvent', 1)
-cmd.set('dot_density', 3)
+cmd.set('dot_density', 3) # <-- Tune coarseness of SASA calc
 
 num_atoms=cmd.count_atoms('all')
 res=np.zeros(1)
 
 for i in range(1, num_atoms+1):
 	print(cmd.get_area('index %d' %i))
-	res=np.append(res,cmd.get_area('index %d' %i))
 
 np.savetxt('areas_results.txt',res)
